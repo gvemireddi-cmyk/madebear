14283caa8d914f7d2e1f5ea9cd54d37081a5bfb0
gvemireddi-cmyk <gvemireddi@gmail.com>
2026-01-31 23:52:35 -0600

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://prospective-orange-mtgy5ysiel.edgeone.app/Ed%20Sheeran%20-%20Perfect%20(Official%20Music%20Video).mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dkvlfvbuq/video/upload/v1769925046/Ed_Sheeran_-_Perfect_Official_Music_Video_odzer5.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
